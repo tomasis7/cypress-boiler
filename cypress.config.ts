47f19ddd1e3dfc671babbc862f0f1bca69efe66e
Tomas Maldonis <tomasis7@gmail.com>
2025-08-28 11:12:34 +0200

    Fix MongoMemoryReplSet instantiation and add server spawn for Cypress tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,14 +2,17 @@ import { defineConfig } from "cypress";
 import { db } from "./prisma/db";
 import { seedTodos } from "./prisma/seed/todo";
 import { MongoMemoryReplSet } from "mongodb-memory-server";
+import { spawn } from "child_process";
 
 export default defineConfig({
   e2e: {
     async setupNodeEvents(on, config) {
       //1. skapa in memory databas (replica set prisma gn'ller annars)
-      const db = await MongoMemoryReplSet().create( { replSet: { count: 1}})
+      const db = await MongoMemoryReplSet.create({ replSet: { count: 1}})
       const dbUri = db.getUri("cypress-test");
+    
       //2. nextjs starta srv pa annat port som ansluter till 1.
+      const server = spawn("npx", ["next", "dev", "--turbopack", '-port', '3100'], { env: { NODE_ENV: "test", DATABASE_URL: dbUri }, stdio: "inherit" });
       //3. vanta p[ att nextjs serv startat igang innan cypress kor vidare
       //4. stada upp processerna dvs mongo databasen o srv  nextjs
       //5. reseeda om db 
