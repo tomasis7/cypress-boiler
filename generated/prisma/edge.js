a09500d623b52b3222b5b5e2aa6de17d36112a14
Tomas Maldonis <tomasis7@gmail.com>
2025-09-02 13:50:28 +0200

    chore: remove postinstall flag from Prisma configuration

--- generated/prisma/edge.js
+++ generated/prisma/edge.js
@@ -153,7 +153,6 @@ const config = {
     "db"
   ],
   "activeProvider": "mongodb",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- generated/prisma/index.js
+++ generated/prisma/index.js
@@ -154,7 +154,6 @@ const config = {
     "db"
   ],
   "activeProvider": "mongodb",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
